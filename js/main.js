94794562f304ffd87fb337fac4672a0be0b2b6ac
Abdul Aziz <aazizmajid2@gmail.com>
2018-08-03 01:27:37 +0300

    Add accomadation details html file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+$(window).on('load', function() {
+    $('#collapseOne').collapse({toggle: true});
+    $('#collapseTwo').collapse({toggle: true});
+    $('#collapseThree').collapse({toggle: true});
+    $('#collapseFour').collapse({toggle: true});
+    $('#collapseFive').collapse({toggle: true});
+})
+
 var myId = localStorage.getItem('idHajj');
-var myId = "988746";
 
 if (myId === null) {
-    myId = " "
+  myId = " "
 }
 
 
@@ -11,45 +18,12 @@ var mutawwifArr = mutawwifInfo.completeInfo;
 var locationsArr = geolocation.pilgrimsLocationCollection;
 
 var filterData = personArr.filter(function (obj) {
-    return obj.uniqueID == myId;
+  return obj.uniqueID == myId;
 });
-
-var y = filterData.map(function (r) {
-    return r.firstName + ' ' + r.lastName;
-});
-var y1 = filterData.map(function (r) {
-    return r.uniqueID;
-});
-var personalImg = filterData.map(function (r) {
-    return r.imagePath;
-});
-console.log(y.toString());
-console.log(y1.toString());
-console.log(personalImg.toString());
-
-
-$('.profile-usertitle-name').html(y);
-$('.profile-usertitle-job').text(y1);
-$(".img-haaji").attr("src", personalImg);
-$(".show-direction-btn").click(function () {
-    //alert("Hello! I am an alert box!");
-    //$("#map").css('height', '0');
-    $("#map").hide();
-    //saveSettings();
-    //window.location.href = 'mapRouteWithoutMap.html';
-
-});
-
-function loadSettings() {
-    $('#start').val(localStorage.start);
-    $('#end').val(localStorage.end);
-}
-
 
 var mutawwifId = filterData.map(function (r) {
   return r.mutawwifID;
 });
-
 
 
   var locationFilterData = locationsArr.filter(function(obj) {
@@ -57,9 +31,9 @@ var mutawwifId = filterData.map(function (r) {
   });
 
 
-  console.log(fullName.toString());
+  /*console.log(fullName.toString());
   console.log(y1.toString());
-  console.log(personalImg.toString());
+  console.log(personalImg.toString());*/
 
   
 
@@ -104,13 +78,12 @@ var nationality = filterData.map(function (r) {
 });
 
 
-
 $('.profile-usertitle-name').html(fullName);
 $('.profile-usertitle-job').text(y1);
 
 $(".img-haaji").attr("src", personalImg);
 $(".show-direction-btn").click(function () {
-    window.location.href = 'mapRouteWithoutMap.html';
+  window.location.href = 'mapRouteWithoutMap.html';
 });
 
 
@@ -126,82 +99,41 @@ $('.profile-mutawwifName').html(mutawwifName);
 
 /*Goolge Map API Functions*/
 function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 21.42, lng: 39.82}
-    });
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 21.42, lng: 39.82}
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('right-panel'));
 
-    var control = document.getElementById('floating-panel');
-    control.style.display = 'block';
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+  var control = document.getElementById('floating-panel');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-    var onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('start').addEventListener('change', onChangeHandler);
-    document.getElementById('end').addEventListener('change', onChangeHandler);
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  directionsService.route({
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  }, function (response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
 
+/*Goolge Map API Functions*/
 
-
-function saveSettings() {
-    localStorage.start = $('#start').val();
-    localStorage.end = $('#end').val();
-}
-
-function initMap() {
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 21.42, lng: 39.82}
-    });
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
-
-    var control = document.getElementById('floating-panel');
-    control.style.display = 'block';
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-
-    var onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('start').addEventListener('change', onChangeHandler);
-    document.getElementById('end').addEventListener('change', onChangeHandler);
-}
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
-}
