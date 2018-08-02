de53ecf35f40be312ba34de1ed0b0bf98b3ba2ea
arahmanmdmajid <42006410+arahmanmdmajid@users.noreply.github.com>
2018-08-02 05:28:39 +0300

    Add changes

---
+++ DB/pilgrimLocationCollection.js
@@ -0,0 +1,64 @@
+var locationinfo = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "ire3k295",
+        "title": "Cornell University",
+        "description": "<div style=\"width:330px;height:240px;overflow:auto\"><div class=\"googft-info-window\" style=\"font-family:sans-serif\"> <b>Governmental Body/Entity:</b> Cornell University<br><p> <b>Type of Drone:</b> University Built (One-Third Scale Piper Cub) UAS</p><p><b>Status:</b> Expired</p><p> <b>General Location of Drone Activity:</b> Aurora, NY</p><p> <b>Stated objective/purpose of COA:</b> The purpose of the proposed UAV flights in this COA is to develop and use experimentally, a system to vertically profile the atmosphere from 300 ft agl to 3000 ft agl.  The UAV payload will be instrumentation to continuously measure temperature, relative humidity, wind speed and wind direction as the UAV spirals vertically from 300 ft agl to 3000 ft agl and back down to 300 ft agl.  The UAV will maintain a GPS controlled circle with a 1500 ft diameter utilizing a Micropilot 1028 autopilot.  The climb rate will be 150 ft/min and the duration of the flight will be approximately 40 min. This system will replace the release of helium filled balloons with radio equipped weather packages and the use of tethered blimps utilized to lift weather instrumentation into the lower atmosphere.   An UAV mounted system is superior because the helium filled free flying balloons rise through the lower 3000 ft of the atmosphere too quickly for accurate weather data.  The tethered blimps have a FAA imposed altitude restriction of 1000-1500 ft due to their danger to aircraft and the inability of the operators to react quickly enough to avoid full scale aircraft.  In contrast, a UAV mounted system can spiral up to 3000 ft in a relatively short time period, yielding high quality weather measurements and can quickly be diverted to avoid full scale aircraft which enter into the area of UAV flights.</p><p> <b>Effective Dates:</b> 3/1/2010-2/28/2011</p><p> <b>Comments:</b> </p><p><b>Link to Records:</b> <a href=\"https://www.eff.org/document/cornell-university-drone-records\" target=\"_blank\">https://www.eff.org/document/cornell-university-drone-records</a></p> </div></div>",
+        "marker-color": "#00bcce",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "marker-zoom": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.702448,
+          42.753959
+        ]
+      },
+      "id": "ci9bzhum74haejrlvrn7i8fep"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "8v3nbjxf",
+        "title": "US Marine Corps - 2011-ESA-25-COA",
+        "description": "<div style=\"width:330px;height:240px;overflow:auto\"><div class=\"googft-info-window\" style=\"font-family:sans-serif\"> <b>Governmental Body/Entity:</b> US Marine Corps - 2011-ESA-25-COA<br><p> <b>Type of Drone:</b> RQ-7B Shadow</p><p><b>Status:</b> Active</p><p> <b>General Location of Drone Activity:</b> Carteret, NC</p><p> <b>Stated objective/purpose of COA:</b> The USMC has a requirement to operate its Shadow 200 UAS RQ-7B aircraft at MCALF Bogue in support of aircrew training, expeditionary site setup, and integration into large force exercises at Camp Lejeune.  This requires the system to transit from 0 - 1200 ft within the class D airspace and into the R5306C restricted airspace in order to properly train squadron aircrews in the effective employment of the new system and to transit to the other adjoining restricted airspace. VMU-2 will fly an average of three (3) to four (4) training flights per day while conducting operations out of MCALF Bogue.  All flights will strictly adhere to a pre-established UAV pattern within the lateral confines of the Class D airspace while using a ground/tower observer until established in the R5306C restricted airspace.  Operations will be conducted Monday through Friday.  Each flight will last approximately 1.5 to 2 hours.</p><p> <b>Effective Dates:</b> 11/1/2011-10/31/2012</p><p> <b>Comments:</b> Docs withheld under exemption 3 (22 USC 2751) (this statute is about foreign arms sales & information subject to arms export controls. Section 2778(e) allows information to be withheld under FOIA): - 19 pages withheld from \"COMMUNICATIONS.pdf\" - 22 pp w/h from \"CONTROL_STATION.pdf\" - 1 pp w/h from \"AIRCRAFT_SYSTEM.bmp\" - 20 pp w/h from \"LAUNCH_RECOVERY.pdf\"</p><p><b>Link to Records:</b> <a href=\"https://www.eff.org/document/us-marines\" target=\"_blank\">https://www.eff.org/document/us-marines</a></p> </div></div>",
+        "marker-color": "#f63a39",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "marker-zoom": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.548823,
+          34.901926
+        ]
+      },
+      "id": "ci9bzhum84hafjrlv8dgd8w2h"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "3udrdq3j",
+        "title": "Air Force - 2011-WSA-55-COA",
+        "description": "<div style=\"width:330px;height:240px;overflow:auto\"><div class=\"googft-info-window\" style=\"font-family:sans-serif\"> <b>Governmental Body/Entity:</b> Air Force - 2011-WSA-55-COA<br><p> <b>Type of Drone:</b> Wasp (BATMAV) Small UAS</p><p><b>Status:</b> </p><p> <b>General Location of Drone Activity:</b> Camp Rilea, OR</p><p> <b>Stated objective/purpose of COA:</b> Objective is to provide operator currency for the 125th Special Tactics Squadron personnel using the Wasp (BATMAV) Small UAS.</p><p> <b>Effective Dates:</b> 8/5/2011-8/4/2012</p><p> <b>Comments:</b> </p><p><b>Link to Records:</b> <a href=\"https://www.eff.org/document/air-force-2011\" target=\"_blank\">https://www.eff.org/document/air-force-2011</a></p> </div></div>",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "airport",
+        "marker-zoom": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.931532,
+          46.116494
+        ]
+      },
+      "id": "ci9bzhuma4hagjrlvmb8ywlzl"
+    }
+}
