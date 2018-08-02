6d320c24477dd1e71e70a15ad9ee7ff465679618
arahmanmdmajid <42006410+arahmanmdmajid@users.noreply.github.com>
2018-08-02 05:32:56 +0300

    Add changes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@ var fullName = personalInfo.personalInfo[0].firstName + ' ' + personalInfo.perso
 
 $('.profile-usertitle-name').text(fullName);
 $('.profile-usertitle-job').text(personalInfo.personalInfo[0].uniqueID);
+
+function initMap() {
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {lat: 21.42, lng: 39.82}
+    });
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+    var control = document.getElementById('floating-panel');
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+    var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    directionsService.route({
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING'
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
