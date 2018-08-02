4c3f1fe6c7dbb5e5203285cba4fc7398e8d7490f
shuvo habib <shuvohabib@gmail.com>
2018-08-03 01:15:15 +0600

    Add

--- DB/personalInfoCollection.js
+++ DB/personalInfoCollection.js
@@ -1,20 +1,28 @@
 var personalInfo = {
-"type":"personalInfoCollection",
-"personalInfo":[
-	{
-		"uniqueID":"891131",
-		"firstName":"Adel",
-		"lastName":"Ali",
-		"nationality":"Saudi",
-		"imagePath":"img/891131.jpg",
-		"mutawwifID":"AB123456"
-	},
-	{
-		"uniqueID":"988746",
-		"firstName":"AbdulAziz",
-		"lastName":"M.Abdul Majid",
-		"nationality":"Pakistani",
-		"imagePath":"img/988746.jpg",
-		"mutawwifID":"BA412364"
-	}]
+  "type": "personalInfoCollection",
+  "personalInfo": [
+    {
+      "uniqueID": " ",
+      "firstName": "You're not Authorized",
+      "lastName": "",
+      "nationality": "",
+      "imagePath": "img/empty.png",
+      "mutawwifID": "123456"
+    },
+    {
+      "uniqueID": "891131",
+      "firstName": "Adel",
+      "lastName": "Ali",
+      "nationality": "Saudi",
+      "imagePath": "img/891131.jpg",
+      "mutawwifID": "AB123456"
+    },
+    {
+      "uniqueID": "988746",
+      "firstName": "AbdulAziz",
+      "lastName": "M.Abdul Majid",
+      "nationality": "Pakistani",
+      "imagePath": "img/988746.jpg",
+      "mutawwifID": "BA412364"
+    }]
 };
--- js/main.js
+++ js/main.js
@@ -1,61 +1,64 @@
 var myId = localStorage.getItem('idHajj');
 
+if (myId === null) {
+  myId = " "
+}
+
 
 var personArr = personalInfo.personalInfo;
 var mutawwifArr = mutawwifInfo.completeInfo;
 
-var filterData = personArr.filter(function(obj) {
-    return obj.uniqueID == myId;
-  });
+var filterData = personArr.filter(function (obj) {
+  return obj.uniqueID == myId;
+});
 
-  var mutawwifId = filterData.map(function(r){
-	   	 return r.mutawwifID;
-  });
-  
-  
-var mutawwifFilterData = mutawwifArr.filter(function(obj) {
-    return obj.uniqueID == mutawwifId;
-  });
- var mutawwifName = mutawwifFilterData.map(function(r){
-	   	 return r.fullName;
-  });
+var mutawwifId = filterData.map(function (r) {
+  return r.mutawwifID;
+});
 
-  var fullName = filterData.map(function(r) {
-    return r.firstName + ' '+ r.lastName;
-  });
-  var y1 = filterData.map(function(s) {
-    return s.uniqueID;
-  });
-  var personalImg = filterData.map(function(t) {
-    return t.imagePath;
-  });
 
-  console.log(fullName.toString());
-  console.log(y1.toString());
-  console.log(personalImg.toString());
-  
+var mutawwifFilterData = mutawwifArr.filter(function (obj) {
+  return obj.uniqueID == mutawwifId;
+});
+var mutawwifName = mutawwifFilterData.map(function (r) {
+  return r.fullName;
+});
 
-  var nationality = filterData.map(function(r){
-	   return r.nationality;
-  });
-  
+var fullName = filterData.map(function (r) {
+  return r.firstName + ' ' + r.lastName;
+});
+var y1 = filterData.map(function (s) {
+  return s.uniqueID;
+});
+var personalImg = filterData.map(function (t) {
+  return t.imagePath;
+});
 
-  /*for testing only*/
- console.log(mutawwifFilterData);
- console.log(mutawwifId.toString());
-  
-   /*for testing only*/
-  
+console.log(fullName.toString());
+console.log(y1.toString());
+console.log(personalImg.toString());
+
+
+var nationality = filterData.map(function (r) {
+  return r.nationality;
+});
+
+
+/*for testing only*/
+console.log(mutawwifFilterData);
+console.log(mutawwifId.toString());
+
+/*for testing only*/
+
 /*profile.html*/
 
 $('.profile-usertitle-name').html(fullName);
 $('.profile-usertitle-job').text(y1);
+
 $(".img-haaji").attr("src", personalImg);
-
 $(".show-direction-btn").click(function () {
-    window.location.href='mapRouteWithoutMap.html';
+  window.location.href = 'mapRouteWithoutMap.html';
 });
-
 
 
 /*info.html*/
@@ -70,40 +73,41 @@ $('.profile-mutawwifName').html(mutawwifName);
 
 /*Goolge Map API Functions*/
 function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 21.42, lng: 39.82}
-    });
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 21.42, lng: 39.82}
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('right-panel'));
 
-    var control = document.getElementById('floating-panel');
-    control.style.display = 'block';
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+  var control = document.getElementById('floating-panel');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-    var onChangeHandler = function() {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('start').addEventListener('change', onChangeHandler);
-    document.getElementById('end').addEventListener('change', onChangeHandler);
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    }, function(response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  directionsService.route({
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  }, function (response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
+
 /*Goolge Map API Functions*/
 
