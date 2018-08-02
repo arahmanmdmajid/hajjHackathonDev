c5d5d6eb26912879da65a7ba2c9fead329a2c6ac
shuvo habib <shuvohabib@gmail.com>
2018-08-02 07:16:50 +0600

    Add json data

---
+++ DB/personalInfoCollection.js
@@ -0,0 +1,9 @@
+var personalInfo = {
+"type":"personalInfoCollection",
+"personalInfo":[{
+		"uniqueID":"AB00001",
+		"firstName":"Marium",
+		"lastName":"Islam",
+		"nationality":"Pakistani"
+	}]
+};
---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log(personalInfo.personalInfo[0].firstName);
+
+
+var fullName = personalInfo.personalInfo[0].firstName + ' ' + personalInfo.personalInfo[0].lastName;
+
+$('.profile-usertitle-name').text(fullName);
