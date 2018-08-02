546766e8af444553bc0bcacc656a9381a2de3091
Abdul Aziz <aazizmajid2@gmail.com>
2018-08-02 19:49:08 +0300

    Changed DB data

--- DB/pilgrimLocationCollectionSimple.js
+++ DB/pilgrimLocationCollectionSimple.js
@@ -1,14 +1,38 @@
 const markers = [
     {
-        "name": "Rixos The Palm Dubai",
+		"locId":"loc123",
+        "name": "Arafat Location",
+		"description":"Near Rahmah mount route 3",
+		"category":"arafat",
         "position": [25.1212, 55.1535],
     },
     {
-        "name": "Shangri-La Hotel",
+		"locId":"loc567",
+        "name": "Mina Location",
+		"description":"Near Jamarat route 5",
+		"category":"mina",
         "location": [25.2084, 55.2719]
     },
     {
-        "name": "Grand Hyatt",
+		"locId":"loc890",
+        "name": "Muzdalifah Location",
+		"description":"Near Mashair Maousque route 10",
+		"category":"muzdalifah",
+        "location": [25.2285, 55.3273]
+    },
+    {
+		"locId":"loc112",
+        "name": "Makkah Location",
+		"category":"makkah",
+		"description":"Near Aziz Mousque, street 15, Nuzhah",
+        "location": [25.2285, 55.3273]
+    }
+	,
+    {
+		"locId":"loc134",
+        "name": "Medinah Location",
+		"category":"medinah",
+		"description":"Near Bilal Mousque, street 9, Awali",
         "location": [25.2285, 55.3273]
     }
 ];
