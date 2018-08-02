c934d6f55f41e9e00245dba966118c3f6c6a44ea
Abdul Aziz <aazizmajid2@gmail.com>
2018-08-02 04:24:39 +0300

    Changes in profile file

--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ console.log(personalInfo.personalInfo[0].firstName);
 var fullName = personalInfo.personalInfo[0].firstName + ' ' + personalInfo.personalInfo[0].lastName;
 
 $('.profile-usertitle-name').text(fullName);
+$('.profile-usertitle-job').text(personalInfo.personalInfo[0].uniqueID);
