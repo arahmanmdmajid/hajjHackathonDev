675e0f3d088394b3489ca7449075c7b7705930b5
arahmanmdmajid <42006410+arahmanmdmajid@users.noreply.github.com>
2018-08-02 22:40:10 +0300

    Add changes

---
+++ DB/pilgrimLocationCollection_updated.js
@@ -0,0 +1,104 @@
+{
+"pilgrimsLocationCollection" : [
+	{
+	"userId":"891131",
+	"pilgrimLocation":[
+			{
+				"locID":"loc567",
+				"category":"makkah",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"medina",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"mina",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"mudalifa",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"arafat",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			}	
+		]
+	},
+	{
+	"userId":"891131",
+	"pilgrimLocation":[
+			{
+				"locID":"loc567",
+				"category":"makkah",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"medina",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"mina",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"mudalifa",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			},
+			{
+				"locID":"loc567",
+				"category":"arafat",
+				"geometry": {
+					"type": "Point",,
+					"lat":"-76.702448"
+					"long":"42.753959"
+			  }
+			}	
+		]
+	}
+]
+}
