dd661fb246668b0e0656a530829e12b7b51a638c
shuvo habib <shuvohabib@gmail.com>
2018-08-03 04:49:06 +0600

    Add back functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 var myId = localStorage.getItem('idHajj');
 
-if (myId === null) {
-    myId = " "
-}
-
-
 var personArr = personalInfo.personalInfo;
 var mutawwifArr = mutawwifInfo.completeInfo;
 var locationsArr = geolocation.pilgrimsLocationCollection;
@@ -13,30 +8,23 @@ var filterData = personArr.filter(function (obj) {
     return obj.uniqueID == myId;
 });
 
-var y = filterData.map(function (r) {
+var fullName = filterData.map(function (r) {
     return r.firstName + ' ' + r.lastName;
 });
-var y1 = filterData.map(function (r) {
+var uniqueID = filterData.map(function (r) {
     return r.uniqueID;
 });
 var personalImg = filterData.map(function (r) {
     return r.imagePath;
 });
-console.log(y.toString());
-console.log(y1.toString());
-console.log(personalImg.toString());
 
 
-$('.profile-usertitle-name').html(y);
-$('.profile-usertitle-job').text(y1);
+
+$('.profile-usertitle-name').html(fullName);
+$('.profile-usertitle-job').text(uniqueID);
 $(".img-haaji").attr("src", personalImg);
 $(".show-direction-btn").click(function () {
-    //alert("Hello! I am an alert box!");
-    //$("#map").css('height', '0');
     $("#map").hide();
-    //saveSettings();
-    //window.location.href = 'mapRouteWithoutMap.html';
-
 });
 
 function loadSettings() {
@@ -56,15 +44,6 @@ var mutawwifId = filterData.map(function (r) {
   });
 
 
-  console.log(fullName.toString());
-  console.log(y1.toString());
-  console.log(personalImg.toString());
-
-  
-
-  var nationality = filterData.map(function(r){
-	   return r.nationality;
-  });
   
 var pilgrimLocations = locationFilterData.map(function(r){
     return r.pilgrimLocation;
@@ -83,35 +62,6 @@ var mutawwifName = mutawwifFilterData.map(function (r) {
   return r.fullName;
 });
 
-var fullName = filterData.map(function (r) {
-  return r.firstName + ' ' + r.lastName;
-});
-var y1 = filterData.map(function (s) {
-  return s.uniqueID;
-});
-var personalImg = filterData.map(function (t) {
-  return t.imagePath;
-});
-
-console.log(fullName.toString());
-console.log(y1.toString());
-console.log(personalImg.toString());
-
-
-var nationality = filterData.map(function (r) {
-  return r.nationality;
-});
-
-
-
-$('.profile-usertitle-name').html(fullName);
-$('.profile-usertitle-job').text(y1);
-
-$(".img-haaji").attr("src", personalImg);
-$(".show-direction-btn").click(function () {
-    window.location.href = 'mapRouteWithoutMap.html';
-});
-
 
 /*info.html*/
 $('.profile-usertitle-name-info').html(fullName);
@@ -127,50 +77,6 @@ $('.profile-mutawwifName').html(mutawwifName);
 function initMap() {
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 21.42, lng: 39.82}
-    });
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
-
-    var control = document.getElementById('floating-panel');
-    control.style.display = 'block';
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-
-    var onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('start').addEventListener('change', onChangeHandler);
-    document.getElementById('end').addEventListener('change', onChangeHandler);
-}
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
-}
-
-
-
-function saveSettings() {
-    localStorage.start = $('#start').val();
-    localStorage.end = $('#end').val();
-}
-
-function initMap() {
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsService = new google.maps.DirectionsService;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: {lat: 21.42, lng: 39.82}
