58cfdb5fddac6abb5fee2c2e8d468afaa672a8cf
shuvo habib <shuvohabib@gmail.com>
2018-08-02 18:28:23 +0600

    Add new scanner

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+function onDeviceReady() {
+  $('#scan').click( function()
+      {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+              localStorage.setItem("id", result.text);
+              window.location = "profile.html";
+
+            },
+            function (error) {
+              alert("Scanning failed: " + error);
+            });
+      }
+  );
+}
